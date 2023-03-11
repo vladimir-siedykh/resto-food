f862fe172248071c36a78f03bccde3f2e23ae514
volodymyr-siedykh <datrimius@gmail.com>
2023-03-11 19:49:11 +0200

    v2.4 sitemap

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,15 +1,21 @@
-import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
-        <title>Restofood - Italian Restaurant</title>
-        <meta name='description' content='Italian Restaurant' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+      <NextSeo
+        title='Restofood - First Restaurant in the World'
+        description='Restofood is the first restaurant in the world that serves food that is made from the waste of other restaurants.'
+        canonical='http://www.restofood.store'
+        openGraph={{
+          url: 'http://www.restofood.store',
+          title: 'Restofood - First Restaurant in the World',
+          description:
+            'Restofood is the first restaurant in the world that serves food that is made from the waste of other restaurants.',
+        }}
+      />
       <>
         <Header />
         <main className=''>{children}</main>
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'http://www.restofood.store',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
